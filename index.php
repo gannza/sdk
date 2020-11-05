3ce91b45b58e46e46011a6d1df15bc030bbbd57f
Ganza respice <respinho2014@gmail.com>
2020-11-06 00:14:09 +0200

    ..

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://yegobox.com/sdk/styles.css">
+<style>
+  .button1{
+    background-color: brown;
+  }
+</style>
+</head>
+<body class="mat-typography" style="margin:200px">
+
+  <payment-sdk  
+  enablemomo='true' 
+  showbutton='false' modal="md" 
+  amount='100' 
+  action='Donate' 
+  timeout="1000"
+  currency="RWF",
+  is_creator_account="false",
+  account_id=""
+  enableredirect="true"
+  redirecturl="https://yegobox.com/"
+  ></payment-sdk>
+<script src="https://yegobox.com/sdk/payment-sdk.js" type="text/javascript"></script>
+</body>
+
+<script>
+const el = document.querySelector('payment-sdk');
+
+el.addEventListener('response', (event) => console.log("html",event.detail));
+</script>
+
+</html>
